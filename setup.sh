28b0c481b606d9cab66c893c57916f3fdb7a859b
mmkonrad <M.M.Konrad@web.de>
2016-10-12 13:18:26 +0000

    little adjustments in node_reset.sh and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
+HOME_ADDRESS=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+
 #Setup Infrastructure
 cd "infrastructure-installation"
-docker-compose -H :4000 up -d
+sudo docker-compose -H $HOME_ADDRESS:4000 up -d
 
 #Attach Rabbit-Instances
 i=1
 while [[ $i -le $1 ]]
 do
-    sudo docker -H :4000 run --name rabbit$i --hostname=rabbit$i --net-alias=rabbit  --net=reservierung -e CLUSTERED=true -e CLUSTER_WITH=rabbitroot -d bachelorproject/rabbitcluster
+    sudo docker -H $HOME_ADDRESS:4000 run --name rabbit$i --hostname=rabbit$i --net-alias=rabbit  --net=reservierung -e CLUSTERED=true -e CLUSTER_WITH=rabbitroot -d bachelorproject/rabbitcluster
     ((i = i + 1))
 done
 
@@ -14,7 +18,7 @@ done
 i=1
 while [[ $i -le $1 ]]
 do
-    sudo docker -H :4000 run --name cassandra-$i  --net-alias=cassandra  --net=reservierung -e CASSANDRA_BROADCAST_ADDRESS=cassandra-$i -e CASSANDRA_SEEDS=cassandraroot -d cassandra
+    sudo docker -H $HOME_ADDRESS:4000 run --name cassandra-$i  --net-alias=cassandra  --net=reservierung -e CASSANDRA_BROADCAST_ADDRESS=cassandra-$i -e CASSANDRA_SEEDS=cassandraroot -d cassandra
     ((i = i + 1))
 done
 
@@ -22,7 +26,7 @@ done
 i=1
 while [[ $i -le $1 ]]
 do
-    sudo docker -H :4000 run --name seat_database_copy$i  --net-alias=seat_database_copy  --net=reservierung -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=seatmanagement -d bachelorproject/seat_database_copy
+    sudo docker -H $HOME_ADDRESS:4000 run --name seat_database_copy$i  --net-alias=seat_database_copy  --net=reservierung -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=seatmanagement -d bachelorproject/seat_database_copy
     ((i = i + 1))
 done
 
@@ -31,16 +35,16 @@ sleep 30
 cd "testdata"
 #Initialize data
 
-sudo docker -H :4000 exec -i seat_database mysql -uroot -proot < seat_init.sql
-sudo docker -H :4000 exec -i customer_database mysql -uroot -proot < customer_init.sql
-sudo docker -H :4000 exec -i cassandraroot cqlsh < booking_init.cql
+sudo docker -H $HOME_ADDRESS:4000 exec -i seat_database mysql -uroot -proot < seat_init.sql
+sudo docker -H $HOME_ADDRESS:4000 exec -i customer_database mysql -uroot -proot < customer_init.sql
+sudo docker -H $HOME_ADDRESS:4000 exec -i cassandraroot cqlsh < booking_init.cql
 
 #Init Copies
 
 i=1
 while [[ $i -le $1 ]]
 do
-    sudo docker -H :4000 exec -i seat_database_copy$i mysql -uroot -proot < seat_copy.sql
+    sudo docker -H $HOME_ADDRESS:4000 exec -i seat_database_copy$i mysql -uroot -proot < seat_copy.sql
     ((i = i + 1))
 done
 
@@ -50,4 +54,4 @@ sleep 20
 cd "../../services-setup"
 
 #scale services
-docker-compose -H :4000 scale seatoverviewservice=$1 seatmanagementservice=$1 bookingservice=$1 customermanagementservice=$1 pricingservice=3 ui=$1
+sudo docker-compose -H $HOME_ADDRESS:4000 scale seatoverviewservice=$1 seatmanagementservice=$1 bookingservice=$1 customermanagementservice=$1 pricingservice=3 ui=$1
